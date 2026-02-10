cf6ea5c3d5d70a8a7d80dfa3bc1d027417822f70
Kumar, Ashish (ASINAFSDC) <ashish.kumar15@cgi.com>
2026-02-10 13:18:33 +0530

    Lastest

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SingleCategoryComponent } from './pages/single-category/single-category
 import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { AboutComponent } from './pages/about/about.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
 
 export const routes: Routes = [
   {path : '', component :HomeComponent}, 
   {path : 'category', component :SingleCategoryComponent},
-  {path: 'post' , component :SingleCategoryComponent},
+  {path: 'post' , component :SinglePostComponent},
 
   {path :'about', component :  AboutComponent},
   {path: 'terms-conditions' , component :TermsAndConditionComponent},
